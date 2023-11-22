65e3f2a8f06dd0a3412799321b8637382e4938a1
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-11-22 15:25:44 +0100

    add suspense fallback adn skeleton for revenue

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from '../../lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   //You can either fetch all data and store the data into a variable or desctruct the values and place them around.
   // const cardData = await fetchCardData();
@@ -36,7 +33,10 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        {/* <RevenueChart revenue={revenue} /> */}
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
